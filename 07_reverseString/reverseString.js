2d8b10c93642178519c9bfa4c1b1cd4e56586d69
Matthew <12345689+05schmidtm18@gmail.com.github.com>
2025-07-31 22:16:57 -0400

    adding exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let newSTR = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newSTR += str.charAt(i);
+    }
+    return newSTR;
 };
 
 // Do not edit below this line
