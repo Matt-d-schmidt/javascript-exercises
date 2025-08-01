95f0a155fd835afaf1d3f227ad9a48e909bad016
Matthew <12345689+05schmidtm18@gmail.com.github.com>
2025-08-01 12:08:51 -0400

    adding exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (sNum, eNum) {
+    if (Number.isInteger(sNum) === false || Number.isInteger(eNum) === false || typeof sNum !== 'number' || typeof eNum !== 'number' || sNum < 0 || eNum < 0) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    if (eNum > sNum) {
+        for (let i = sNum; i <= eNum; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = eNum; i <= sNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
