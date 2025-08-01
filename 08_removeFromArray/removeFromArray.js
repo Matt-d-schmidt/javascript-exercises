0a2bf6486afdc10f1918b069969a15e426c3f48b
Matthew <12345689+05schmidtm18@gmail.com.github.com>
2025-08-01 11:56:25 -0400

    Adding exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (ar, num, num2, num3, num4) {
+    for (let i = 0; i < ar.length; i++) {
+
+        if (ar[i] === num) {
+            ar.splice(i, 1);
+            i--; // Adjust index after removal
+        }
+        if (num2 !== undefined && ar[i] === num2) {
+            ar.splice(i, 1);
+            i--; // Adjust index after removal
+        }
+        if (num3 !== undefined && ar[i] === num3) {
+            ar.splice(i, 1);
+            i--; // Adjust index after removal
+        }
+        if (num4 !== undefined && ar[i] === num4) {
+            ar.splice(i, 1);
+            i--; // Adjust index after removal
+        }
+    }
+    return ar;
 };
 
 // Do not edit below this line
