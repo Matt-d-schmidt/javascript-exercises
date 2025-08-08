5247ba5ca38d80137f518668e32f716e5259654d
Matt-d-schmidt <12345689+05schmidtm18@gmail.com.github.com>
2025-08-07 20:54:36 -0400

    adding exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let result = 1;
+  for (let i = 1; i <= num2; i++) {
+    result *= num1;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
