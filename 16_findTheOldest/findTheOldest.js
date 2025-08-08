e6ed15a54450a270e4889d1043059f87b08ffeac
Matt-d-schmidt <12345689+05schmidtm18@gmail.com.github.com>
2025-08-07 21:21:17 -0400

    adding exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        const age = person.yearOfDeath - person.yearOfBirth;
+        return age > oldest.age ? { name: person.name, age } : oldest;
+    }, { name: '', age: 0 });
 };
 
 // Do not edit below this line
