c9cc09f68a64d096dcf2a9b92021f74cb9c74020
Matt-d-schmidt <12345689+05schmidtm18@gmail.com.github.com>
2025-08-07 21:05:37 -0400

    adding exercise 15

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num === 'string') {
+        num = parseInt(num, 10);
+    }
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    let a = 0, b = 1, temp;
+
+    for (let i = 0; i < num; i++) {
+        temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return a;
 };
 
 // Do not edit below this line
